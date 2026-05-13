5addbae2eaa38c79958a0cdd6c721ea7bf5e14df
Mazen Alhadah <mmazn0300@gmail.com>
2026-05-13 19:26:50 +0300

    Add files via upload

--- algo_project/src/algo_project/Sec_Sol.java
+++ algo_project/src/algo_project/Sec_Sol.java
@@ -1,24 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package algo_project;
-
-import java.util.Arrays;
-
-/**
- *
- * @author MazenAlhadah
- */
 public class Sec_Sol {
-    
-    // With Solting
+
+    public static void mergeSort(int[] A, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(A, left, mid);
+            mergeSort(A, mid + 1, right);
+            merge(A, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] A, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = A[left + i];
+        for (int i = 0; i < n2; i++)
+            R[i] = A[mid + 1 + i];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j])
+                A[k++] = L[i++];
+            else
+                A[k++] = R[j++];
+        }
+
+        while (i < n1)
+            A[k++] = L[i++];
+        while (j < n2)
+            A[k++] = R[j++];
+    }
+
     public static long WithSorting(int[] A) {
-        Arrays.sort(A);
+        mergeSort(A, 0, A.length - 1);
         int n = A.length;
 
-        long option1 = A[n - 1] * A[n - 2] * A[n - 3];
-        long option2 = A[0] * A[1] * A[n - 1];
+        long option1 = (long) A[n - 1] * A[n - 2] * A[n - 3];
+        long option2 = (long) A[0] * A[1] * A[n - 1];
 
         return Math.max(option1, option2);
     }
